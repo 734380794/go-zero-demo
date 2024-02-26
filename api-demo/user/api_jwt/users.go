e22b02cbdabaedb46452ee479e82dfa229ce168d
Kevin <734380794@qq.com>
2024-02-26 22:52:18 +0800

    jwt鉴权

--- api-demo/user/api_jwt/users.go
+++ api-demo/user/api_jwt/users.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"go-zero-demo/common/response"
+	"net/http"
 
 	"go-zero-demo/api-demo/user/api_jwt/internal/config"
 	"go-zero-demo/api-demo/user/api_jwt/internal/handler"
@@ -20,7 +23,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(JwtUnauthorizedResult))
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
@@ -28,4 +31,10 @@ func main() {
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
+}
+
+// JwtUnauthorizedResult jwt验证失败的回调
+func JwtUnauthorizedResult(w http.ResponseWriter, r *http.Request, err error) {
+	fmt.Println(err) // 具体的错误，没带token，token过期？伪造token？
+	httpx.WriteJson(w, http.StatusOK, response.Body{10087, nil, "鉴权失败"})
 }
